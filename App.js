00f3abf7553a65a247dcb992fd494d1757accde7
Yui Kim <43528697+yuico@users.noreply.github.com>
2020-01-31 15:47:29 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import BarChart from "./pages/BarChart"
+import './App.css';
+
+// const data = [13, 30, 40, 29, 20];
+
+function App() {
+  const [ data, setData ] = useState([13, 30, 80, 100, 60, 120, 150]);
+  
+  return (
+    <React.Fragment>
+      <BarChart data={data} />
+      <button onClick={() => setData(data.map( value => value + 5 ))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter( value => value <= 30 ))}>
+        Filter data
+      </button>
+      <button onClick={() => setData([...data, Math.round(Math.random() * 100)])}>
+        Add data
+      </button>
+    </React.Fragment>
+  );
+}
+
+export default App;
